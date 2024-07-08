d52087f5ce163d4d09c0e298c15c444251046a7a
灵均 <3301976249@qq.com>
2024-07-08 11:35:03 +0800

    商品浏览业务功能

---
+++ sky-server/src/main/java/com/sky/controller/user/CategoryController.java
@@ -0,0 +1,43 @@
+package com.sky.controller.user;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.sky.entity.Category;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController("userCategoryController")
+@RequestMapping("/user/category")
+@Api(tags = "C端-分类接口")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 查询分类
+     * @param type
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation("查询分类")
+    public Result<List<Category>> list(Integer type) {
+        List<Category> list=new ArrayList<>();
+        if (type!=null) {
+            LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(Category::getType, type);
+             list = categoryService.list(wrapper);
+        }else {
+            list = categoryService.list();
+        }
+        return Result.success(list);
+    }
+}
---
+++ sky-server/src/main/java/com/sky/controller/user/userController.java
@@ -0,0 +1,54 @@
+package com.sky.controller.user;
+
+import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.properties.JwtProperties;
+import com.sky.result.Result;
+import com.sky.service.UserService;
+import com.sky.utils.JwtUtil;
+import com.sky.vo.UserLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Controller
+@Api("c端用户先相关接口")
+@RequestMapping("/user/user")
+public class userController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    /**
+     * 微信登录
+     * @param userLoginDTO
+     * @return
+     */
+    @PostMapping("/login")
+    @ApiOperation("微信登录")
+    public Result<UserLoginVO> login(@RequestBody UserLoginDTO userLoginDTO) {
+        User user=userService.wxLogin(userLoginDTO);
+
+        //生成jwt令牌
+        Map<String,Object> claims=new HashMap<String,Object>();
+        claims.put(JwtClaimsConstant.USER_ID, user.getId());
+        String token = JwtUtil.createJWT(jwtProperties.getUserSecretKey(), jwtProperties.getUserTtl(), claims);
+
+        UserLoginVO result = UserLoginVO.builder()
+                .id(user.getId())
+                .openid(user.getOpenid())
+                .token(token)
+                .build();
+
+        return Result.success(result);
+
+    }
+}
